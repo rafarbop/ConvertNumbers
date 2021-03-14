efdb4910d6433d6f8ce3dc03c7d130b824556147
Rafa Bruno <rafarbop@gmail.com>
2021-03-13 22:50:00 -0300

    finalizado template com bulma

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask('ConvertNumber')
+Flask.jinja_options = {'line_statement_prefix': '#'}
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/decimal')
+def decimal():
+    return render_template('decimal.html')
 
 
 if __name__ == '__main__':
