48eec350e1a605dce6bc518915757d488ad46a3f
Rafa Bruno <rafarbop@gmail.com>
2021-03-16 17:36:51 -0300

    Finalizado - Ok to Deploy
    Falta deploy

--- main.py
+++ main.py
@@ -14,24 +14,40 @@ def index():
 def decimal():
     if request.method == 'POST':
         in_decimal = request.form['number_in_decimal']
-        in_binary = bin(int(request.form['number_in_decimal']))[2:]
-        in_hexa = hex(int(request.form['number_in_decimal']))[2:]
+        in_binary = bin(int(in_decimal))[2:]
+        in_hexa = hex(int(in_decimal))[2:]
         return render_template('decimal.html',
                                in_binary=in_binary,
                                in_decimal=in_decimal,
-                               in_hexa=in_hexa)
+                               in_hexa=in_hexa.upper())
     return render_template('decimal.html')
 
 
-@app.route('/binary')
+@app.route('/binary', methods=["GET", "POST"])
 def binary():
+    if request.method == 'POST':
+        in_binary = request.form['number_in_binary']
+        in_decimal = int(in_binary, base=2)
+        in_hexa = hex(in_decimal)[2:]
+        return render_template('binary.html',
+                               in_binary=in_binary,
+                               in_decimal=in_decimal,
+                               in_hexa=in_hexa.upper())
     return render_template('binary.html')
 
 
-@app.route('/hexadecimal')
+@app.route('/hexadecimal', methods=["GET", "POST"])
 def hexadecimal():
+    if request.method == 'POST':
+        in_hexa = request.form['number_in_hexa']
+        in_decimal = int(in_hexa, base=16)
+        in_binary = bin(in_decimal)[2:]
+        return render_template('hexadecimal.html',
+                               in_binary=in_binary,
+                               in_decimal=in_decimal,
+                               in_hexa=in_hexa.upper())
     return render_template('hexadecimal.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
