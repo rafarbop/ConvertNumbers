730be148e04c6f0d938021117ce4d60feae538a8
Rafael Bruno <rafarbop@gmail.com>
2021-06-25 18:56:06 -0300

    Iniciado novo Index

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=convertnumbers
+FLASK_ENV=development
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='convertnumbers',
+    packages=['convertnumbers'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
